d51ac9bc0cdd8f6c65002b2cb50aeb09943ebbbc
Gabe <jflores353@gmail.com>
2020-07-26 22:52:43 -0500

    Login view created container with Google login button. Still need to incorporate google Oauth. In routes add layout to render layout files.

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,17 @@ const router = express.Router();
 // Login/Landing Page
 // GET /
 router.get('/', (req, res) => {
-	res.render('Login');
+	res.render('login-view', {
+		layout: 'login-layout',
+	});
 });
 
 // Dashboard Page
 // GET /
 router.get('/dashboard', (req, res) => {
-	res.render('Dashboard');
+	res.render('dashboard-view', {
+		layout: 'dashboard-layout',
+	});
 });
 
 module.exports = router;
