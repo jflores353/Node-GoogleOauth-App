3e3ced9f9426d5a424ad0b2936892da303b14b5e
Gabe <jflores353@gmail.com>
2020-07-29 11:07:30 -0500

    Added stories to be rendered in dashboard router

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { ensureGuest, ensureAuth } = require('../middleware/auth');
 
+const Story = require('../models/Stories');
+
 // Login/Landing Page
 // GET /
 router.get('/', ensureGuest, (req, res) => {
@@ -10,18 +12,25 @@ router.get('/', ensureGuest, (req, res) => {
 	});
 });
 
-// Dashboard Page
-// GET /
-router.get('/dashboard', ensureAuth, (req, res) => {
-	res.render('dashboard', {
-		name: req.user.firstName,
-	});
-});
-
 // Logout User
 router.get('/logout', (req, res) => {
 	req.logOut();
 	res.render('/');
+});
+
+// Dashboard Page
+// GET /
+router.get('/dashboard', ensureAuth, async (req, res) => {
+	try {
+		const stories = await Story.find({ user: req.user.id }).lean();
+		res.render('dashboard', {
+			name: req.user.firstName,
+			stories,
+		});
+	} catch (error) {
+		console.error(error);
+		res.render('error/500');
+	}
 });
 
 module.exports = router;
